7563f910f5ca330654fe82e0325c444ad908f7af
Gabriel <gabrieljcs@users.noreply.github.com>
2021-03-29 14:21:35 -0300

    Fix output formatting, add COUNT_ONLY option

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Creates a .csv log of Deep Security computers and its associated IPS rules in th
 hostName,displayName,ipsState,ipsStatus,ipsRulesCount,ipsRulesID,ipsRulesName,ipsRulesSeverity,ipsRulesCVE
 
 There may be multiple ipsRulesIDs and its associated content per computer, and there may be rules with multiple or no CVEs.
+
+Usage:
+------
+Generate an API key and fill it with your manager information inside the script.
+
+If you don't want the rules description, change ```COUNT_ONLY``` to ```True```.
--- deepsecurity-ips-rules.py
+++ deepsecurity-ips-rules.py
@@ -6,10 +6,11 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 API_KEY = ''
-MANAGER_URL = ''
+MANAGER_URL = 'https://cloudone.trendmicro.com/'
 FILENAME = 'output.csv'
+COUNT_ONLY = False
 
-if (API_KEY == '' or MANAGER_URL == ''):
+if not (API_KEY or MANAGER_URL):
     print("Fill the API_KEY and MANAGER_URL fields inside the script before running.")
     exit(1)
 
@@ -21,9 +22,12 @@ r = requests.get(MANAGER_URL + '/api/computers', headers=headers, verify=False)
 s = json.loads(r.text)
 
 print("Grabbing IPS information...")
-with open(FILENAME, 'w') as outfile:
+with open(FILENAME, 'w', newline='') as outfile:
     wr = csv.writer(outfile, delimiter=',')
-    wr.writerow( ['hostName', 'displayName', 'ipsState', 'ipsStatus', 'ipsRulesCount', 'ipsRulesID', 'ipsRulesName', 'ipsRulesSeverity', 'ipsRulesCVE'] )
+    if (COUNT_ONLY):
+        wr.writerow( ['hostName', 'displayName', 'ipsState', 'ipsStatus', 'ipsRulesCount'] )
+    else:
+        wr.writerow( ['hostName', 'displayName', 'ipsState', 'ipsStatus', 'ipsRulesCount', 'ipsRulesID', 'ipsRulesName', 'ipsRulesSeverity', 'ipsRulesCVE'] )
     for i in s['computers']:
         print('.', end='')
         rowdata = [ i['hostName'], i['displayName'], i['intrusionPrevention']['state'], i['intrusionPrevention']['moduleStatus']['agentStatus'] ]
@@ -32,11 +36,17 @@ with open(FILENAME, 'w') as outfile:
         else:
             t = requests.get(MANAGER_URL + '/api/computers/' + str(i['ID']) + '/intrusionprevention/rules', headers=headers, verify=False)
             u = json.loads(t.text)
-            ipsdata = [ len(u['intrusionPreventionRules']) ]
-            for j in u['intrusionPreventionRules']:
-                ipsdata = ipsdata + [ j['ID'], j['name'], j['severity'] ]
-                if 'CVE' in j:
-                    ipsdata = ipsdata + j['CVE']
-            wr.writerow(rowdata + ipsdata)
+            rulescount = [ len(u['intrusionPreventionRules']) ]
+            if (COUNT_ONLY):
+                wr.writerow(rowdata + rulescount)
+            else:
+                for index, j in enumerate(u['intrusionPreventionRules']):
+                    ipsdata = [ j['ID'], j['name'], j['severity'] ]
+                    if 'CVE' in j:
+                        ipsdata = ipsdata + j['CVE']
+                    if index == 0:
+                        wr.writerow(rowdata + rulescount + ipsdata)
+                    else:
+                        wr.writerow([''] + [''] + [''] + [''] + [''] + ipsdata)
 
 print("Done! Written to " + FILENAME)
